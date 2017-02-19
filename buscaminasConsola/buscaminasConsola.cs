c2cdf4f0740a16b2f55306707127fadc9369e2d1
User <User@WINDEV1612EVAL>
2017-02-19 21:01:12 +0100

    Add project files.

---
+++ buscaminasConsola/buscaminasConsola.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using buscaminasConsola.models;
+
+namespace buscaminasConsola
+{
+    class buscaminasConsola
+    {
+        static void Main(string[] args)
+        {
+            new buscaminasConsola().Comenzar();
+        }
+
+        private void Comenzar()
+        {
+            int n = 5;
+            int m = 8;
+            Tablero tablero = new Tablero(n, m);
+            Console.WriteLine(tablero);
+            Console.WriteLine("Fila 1 -->");
+            int fila = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Columna 1 -->");
+            int columna = Int32.Parse(Console.ReadLine());
+            tablero.LevantaCasilla(fila, columna);
+            Console.WriteLine(tablero);
+            Console.Read();
+
+        }
+
+    }
+}
